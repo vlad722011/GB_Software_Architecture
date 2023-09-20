25d6daee497ab862693bb563760575e5e1e9ba32
Vladislav Bursaev <vlad722011@yandex.ru>
2023-09-20 17:43:16 +0700

    seminar_one

---
+++ README.md
@@ -0,0 +1,1 @@
+# GB_Software_Architecture
---
+++ lectionOne/src/main/java/test/testElement/Car.java
@@ -0,0 +1,29 @@
+package test.testElement;
+
+// Класс (Class) с реализацией интерфейса (Realization)
+public class Car extends Vehicle implements Drivable {
+    FuelType fuelType;
+
+    // Атрибуты (Attributes)
+    private int speed;
+    public String color;
+
+    // Методы (Methods)
+    public void accelerate() {
+        speed += 10;
+    }
+
+    public void brake() {
+        speed -= 10;
+    }
+
+    // Реализация интерфейса
+    public void drive() {
+        System.out.println("Driving...");
+    }
+
+    // Реализация абстрактного метода
+    void start() {
+        System.out.println("Starting the car.");
+    }
+}
